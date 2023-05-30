a1225c6809ece2694df69bf2c42e31542c350f8f
ssangbomb <ssangbomb@gmail.com>
2023-05-30 12:58:20 -0700

    ex2 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.where(id: 1)
+@store2 = Store.second
+puts @store1
+puts @store2
+
+@store1.update(name: "Coquitlam")
